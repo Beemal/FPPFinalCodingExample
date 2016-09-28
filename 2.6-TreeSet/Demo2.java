aabc8e5fdf9e625eb8e788b1f5de57aa90263d41
Julia Chen Jing <juliachenjing@gmail.com>
2016-09-28 17:06:31 -0500

    Create Demo2.java

---
+++ 2.6-TreeSet/Demo2.java
@@ -0,0 +1,67 @@
+
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class Test {
+
+	public static void main(String [] args)
+	{
+		int [] a = {15, 12, 9, 56, 1, 16, 19, 22, 3, 100, 2, 25};
+		
+		TreeSet<Integer> ts = new TreeSet<Integer>();
+		
+		for (int j = 0; j < a.length; j++ )
+		{
+			ts.add(a[j]);
+			System.out.println("\nAfter inserting " + j + "th item " + a[j]);
+			Iterator<Integer> it = ts.iterator();		
+			Integer nextItem = null;
+			while(it.hasNext()) {
+				nextItem = it.next();
+				System.out.print(nextItem + " ");
+			} 
+			System.out.println();
+		}
+		System.out.println();
+	}
+}
+/*
+
+After inserting 0th item 15
+15 
+
+After inserting 1th item 12
+12 15 
+
+After inserting 2th item 9
+9 12 15 
+
+After inserting 3th item 56
+9 12 15 56 
+
+After inserting 4th item 1
+1 9 12 15 56 
+
+After inserting 5th item 16
+1 9 12 15 16 56 
+
+After inserting 6th item 19
+1 9 12 15 16 19 56 
+
+After inserting 7th item 22
+1 9 12 15 16 19 22 56 
+
+After inserting 8th item 3
+1 3 9 12 15 16 19 22 56 
+
+After inserting 9th item 100
+1 3 9 12 15 16 19 22 56 100 
+
+After inserting 10th item 2
+1 2 3 9 12 15 16 19 22 56 100 
+
+After inserting 11th item 25
+1 2 3 9 12 15 16 19 22 25 56 100 
+
+*/
