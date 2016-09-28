fae2ab17ed7f587fa425fe322391d940901cb547
Julia Chen Jing <juliachenjing@gmail.com>
2016-09-28 17:30:15 -0500

    Update Demo2.java

--- 2.7-HashTable/Demo2.java
+++ 2.7-HashTable/Demo2.java
@@ -13,36 +13,52 @@ public class Test {
 	        hm.put("3","THIRD INSERTED");
 	        // hm.put(null,null); Throw run time error - NullPointer Exception
 	        
+	        System.out.println("0all the values:");
 	        Iterator it = (Iterator) hm.keys();
-	        System.out.println("My Hashtable content:");
+	        while(it.hasNext())
+	        {
+	        	System.out.println(hm.get(it.next()));
+	        }
+
+	        System.out.println("\n1 My Hashtable content:");
 	        System.out.println(hm); // Display the contents by key wise reverse order
 	        
 	        //Checking Keys
 	        if(hm.containsKey("1")){
-	            System.out.println("The Hashtable contains key 1");
+	            System.out.println("\n2  The Hashtable contains key 1");
 	        } else {
-	            System.out.println("The Hashtable does not contains key 1");
+	            System.out.println("\n3 The Hashtable does not contains key 1");
 	        }
 	        // Checking Value 
 	        if(hm.containsValue("SECOND INSERTED")){
-	            System.out.println("The Hashtable contains value SECOND INSERTED");
+	            System.out.println("\n5 The Hashtable contains value SECOND INSERTED");
 	        } else {
-	            System.out.println("The Hashtable does not contains value SECOND INSERTED");
+	            System.out.println("\n6 The Hashtable does not contains value SECOND INSERTED");
 	        }
 	        //Clearing Items
 	        
-	        System.out.println("Clearing Hashtable:");
+	        System.out.println("\n7 Clearing Hashtable:");
 	        hm.clear();
-	        System.out.println("Content After clear:");
+	        System.out.println("\n8 Content After clear:");
 	        System.out.println(hm);}
 }
 
 /*
- My Hashtable content:
+0all the values:
+THIRD INSERTED
+SECOND INSERTED
+FIRST INSERTED
+
+1 My Hashtable content:
 {3=THIRD INSERTED, 2=SECOND INSERTED, 1=FIRST INSERTED}
-The Hashtable contains key 1
-The Hashtable contains value SECOND INSERTED
-Clearing Hashtable:
-Content After clear:
+
+2  The Hashtable contains key 1
+
+5 The Hashtable contains value SECOND INSERTED
+
+7 Clearing Hashtable:
+
+8 Content After clear:
 {}
+
  */
