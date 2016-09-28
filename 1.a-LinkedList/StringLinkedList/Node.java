6483c8cae67c478a10c4ce6a69e48fad5824e35e
Julia Chen Jing <juliachenjing@gmail.com>
2016-09-28 15:06:11 -0500

    Create Node.java

---
+++ 1.a-LinkedList/StringLinkedList/Node.java
@@ -0,0 +1,15 @@
+public class Node {
+		String value;
+		Node next;
+		Node previous;
+
+		Node(Node previous, String value, Node next) {
+			this.previous = previous;
+			this.value = value;
+			this.next = next;
+		}
+
+		public String toString() {
+			return value;
+		}
+	}
