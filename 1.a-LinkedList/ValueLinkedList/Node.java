79272257c94ab125b3d2f0d566f6bd65c48a5afe
Julia Chen Jing <juliachenjing@gmail.com>
2016-09-28 14:42:01 -0500

    Create Node.java

---
+++ 1.a-LinkedList/ValueLinkedList/Node.java
@@ -0,0 +1,18 @@
+public class Node {
+
+	Object  value;
+	Node next;
+	Node previous;
+	Node(Node previous,Object value, Node next)
+	{
+		this.next=next;
+		this.previous=previous;
+		this.value=value;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return value +" ";
+	}
+}
