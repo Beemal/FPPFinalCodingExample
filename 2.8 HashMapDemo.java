bc83f4d730da7e29f0cf85f2ed1aac610d0584ec
Julia Chen Jing <juliachenjing@gmail.com>
2016-10-26 17:53:05 -0500

    Update 2.8 HashMapDemo.java

--- 2.8 HashMapDemo.java
+++ 2.8 HashMapDemo.java
@@ -1,6 +1,3 @@
-
-import java.util.Collections;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -14,8 +11,15 @@ public class Test{
 		productMap.put(3, "Systems");
 		productMap.put(7, "Mobile");
 		productMap.put(null, null);
+		System.out.println("HashMap test~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		System.out.println(productMap);
+		System.out.println(productMap.get(3));
+		System.out.println(productMap.containsKey(5));
+		System.out.println(productMap.keySet());
+		System.out.println(productMap.values());
 		
+		
+		 System.out.println("HashSet test~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		HashSet<String> stateSet = new HashSet<String>();
 		stateSet.add ("CA");
 		stateSet.add ("WI");
@@ -42,7 +46,13 @@ public class Test{
 
 }
 /*
+HashMap test~~~~~~~~~~~~~~~~~~~~~~~~~~
 {null=null, 1=Keys, 3=Systems, 4=Books, 7=Mobile}
+Systems
+false
+[null, 1, 3, 4, 7]
+[null, Keys, Systems, Books, Mobile]
+HashSet test~~~~~~~~~~~~~~~~~~~~~~~~~~
 [null, WI, PB, NY, CA]
 Value : 16
 Value : 19
